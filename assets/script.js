d430af6c8a9a2ce9e77c170bc744df9993aace93
Alex Ortega <alexdiclothingco@icloud.com>
2022-04-10 01:28:42 -0600

    Halfway Save

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,10 @@
 var titleEl = document.getElementById('title');
 var startButton = document.getElementById('startBtn');
 var timerEl = document.getElementById('timer-count');
-var questions = document.getElementById('questions').style.display = 'none';
+var question0 = document.getElementById('question0').style.display = 'none';
+var question1 = document.getElementById('question1').style.display = 'none';
+var question2 = document.getElementById('question2').style.display = 'none';
+var question3 = document.getElementById('question3').style.display = 'none';
 var timer = "";
 var timerCount = "";
 
@@ -13,7 +16,7 @@ function startCodeQuiz() {
     timerCount = 60;
     console.log('Started');
 
-    if(startCodeQuiz = 'none'){
+    if (startCodeQuiz = 'none') {
         titleEl.style.display = 'none';
 
         startTimer()
@@ -21,9 +24,47 @@ function startCodeQuiz() {
 }
 
 function startTimer() {
-    timer = setInterval(function() {
-      timerCount--;
-      timerEl.textContent = timer;
-      console.log(timerCount);
+    // Sets timer
+    timer = setInterval(function () {
+        timerCount--;
+        timerEl.textContent = ("Time:" + timerCount + "s");
+        askQuestions();
+        
+        console.log(timerCount);
+
+        if (timerCount >= 0) {
+            // Tests if win condition is met
+            if (isWin && timerCount > 0) {
+                // Clears interval and stops timer
+                clearInterval(timer);
+                winGame();
+            }
+        }
+        // Tests if time has run out
+        if (timerCount === 0) {
+            // Clears interval
+            clearInterval(timer);
+            loseGame();
+        }
     }, 1000);
+}
+
+//function to trigger questions
+function askQuestions() {
+    question0 = document.getElementById("question0");
+    if (question0.style.display === "none") {
+      question0.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
   }
+
+//function for winning
+function winGame(){
+
+}
+
+//function for losing
+function loseGame(){
+
+}
